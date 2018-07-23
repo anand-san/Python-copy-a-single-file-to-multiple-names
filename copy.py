31bbb0263ac06832ecc67a0772b9e5ce0ad78b01
Anand Kumar <seanjin17@gmail.com>
2018-07-23 22:34:12 +0530

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,3 +1,5 @@
+# Author : https://github.com/seanjin17
+
 import shutil
 #Output file names
 id = ("1541012007","1541012014","1541012024","1541012026","1541012034","1541012041","1541012044","1541012046","1541012080","1541012088","1541012096","1541012101","1541012103","1541012104",) 
@@ -6,7 +8,8 @@ src=input("Enter file name with extension (File must be in same path): ")
 dst=input("Enter Output Extension followed by (.) : ")
 for i in id:
 	try:
-		print("copying "+i)
+		print("copying "+i+dst)
 		shutil.copy2(src, i+dst)
 	except:
 		print("Please check Your source file location \n Something went wrong")
+print("Done")
